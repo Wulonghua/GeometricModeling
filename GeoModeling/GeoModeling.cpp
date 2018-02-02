5b72c8d8f52a37327276a0c36c32e6b82b334cfa
Woolongtea <wu.2724@osu.edu>
2018-02-02 15:01:46 -0500

    add dockwidget for curve control.

--- GeoModeling/GeoModeling.cpp
+++ GeoModeling/GeoModeling.cpp
@@ -7,6 +7,7 @@ GeoModeling::GeoModeling(QWidget *parent)
 	ui_control.setupUi(dockw);
 	QDockWidget *dock = new QDockWidget();
 	dock->setWidget(dockw);
+	dock->setMinimumWidth(300);
 	this->addDockWidget(Qt::LeftDockWidgetArea, dock);
 	ui.setupUi(this);
 }
